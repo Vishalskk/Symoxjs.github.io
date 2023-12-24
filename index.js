33cc7cb2a8322faf446bc99413292b2507b706ad
Vishalskk <108940983+Vishalskk@users.noreply.github.com>
2023-12-24 23:44:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,22 +47,6 @@ function darklightthemes(){
 
 
 }
-function lightthemes(){
-    document.getElementById("navdivid").style.backgroundColor="#133131";
-    document.getElementById("navbar").style.backgroundColor="white";
-    document.getElementById("themesun").style.display="none";
-    document.getElementById("div21").style.backgroundColor="white";
-    document.getElementById("div22").style.backgroundColor="white";
-    document.getElementById("div31ka1").style.backgroundColor="white";
-    document.getElementById("div31ka2").style.backgroundColor="white";
-    document.getElementById("div31ka3").style.backgroundColor="white";
-    document.getElementById("div41ka1").style.backgroundColor="white";
-    document.getElementById("div41ka2").style.backgroundColor="white";
-    var nextdsahdivs = document.getElementsByClassName("div1");
-    for(v=0;v<=nextdsahdivs.length;v++){
-        nextdsahdivs[v].style.backgroundColor="white";
-    }
-}
 
 function acro1downclk(){
     document.getElementById("acro1").style.display="block";
@@ -72,6 +56,8 @@ function acro1downclk(){
 
 function acro1upclk(){
     document.getElementById("acro1").style.display="none";
+    document.getElementById("firstupchevronDOP").style.display="none";
+    document.getElementById("firstupchevron").style.display="block";
 }
 
 
@@ -81,3 +67,34 @@ function acro1upclk(){
 
 
 
+function acro2downclk(){
+    document.getElementById("acro2").style.display="block";
+    document.getElementById("secdownchevron").style.display="none";
+    document.getElementById("secupchevron").style.display="block";
+}
+
+function acro2upclk(){
+    document.getElementById("acro2").style.display="none";
+    document.getElementById("secupchevron").style.display="none";
+    document.getElementById("secdownchevron").style.display="block";
+}
+
+
+
+
+function toggleContent(containerId, chevronUpId, chevronDownId) {
+    var container = document.getElementById(containerId);
+
+    if (container.style.display === "none" || container.style.display === "") {
+      container.style.display = "block";
+      document.getElementById(chevronDownId).style.display = "none";
+      document.getElementById(chevronUpId).style.display = "block";
+    } else {
+      container.style.display = "none";
+      document.getElementById(chevronUpId).style.display = "none";
+      document.getElementById(chevronDownId).style.display = "block";
+    }
+  }
+
+
+  
